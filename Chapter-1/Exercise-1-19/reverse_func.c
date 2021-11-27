f9e837b4cffb787ca8eca94ebfe1cd2eae1f0137
SHU GONG <shugong.live@gmail.com>
2021-11-27 15:55:28 +0800

    some practice

--- Chapter-1/Exercise-1-19/reverse_func.c
+++ Chapter-1/Exercise-1-19/reverse_func.c
@@ -2,7 +2,10 @@
 /* Write a function reverse(s) that reverses the character string s. Use it to write a program that reverse its input a line at a time.*/
 
 main() {
-	
+	char[] = line;
+	char[] = reverse_line;
+
+	reverse(line, reverse_line);
 }
 
 int reverse(char[] from, char[] to){
