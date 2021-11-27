b613248b118aeaeb78e2b53b0951e65cc2e2e41c
SHU GONG <shugong.live@gmail.com>
2021-11-27 19:01:12 +0800

    some practice

--- Chapter-1/Exercise-1-19/reverse_func.c
+++ Chapter-1/Exercise-1-19/reverse_func.c
@@ -1,15 +1,38 @@
 #include<stdio.h>
 /* Write a function reverse(s) that reverses the character string s. Use it to write a program that reverse its input a line at a time.*/
 
+#define MAXLEN 1000
+
+void reverse(char from[], char to[]);
 main() {
-	char[] = line;
-	char[] = reverse_line;
+	int c;
+
+	while((c = getchar()) != EOF){
+	char line[MAXLEN];
+	char reverse_line[MAXLEN];
 
 	reverse(line, reverse_line);
+	}
 }
 
-int reverse(char[] from, char[] to){
+void reverse(char from[], char to[]){
 
+	int i, j;
+	j = 0;
+
+	for (i = 0; from[i] != '\0'; i++);
+
+	while (i != 0)	{
+		from[i] = to[j];
+		++j;
+		--i;
+	}
+
+	to[i] = '\0';
+	i = 0;	
+	while(to[i] != '\0'){
+		putchar(to[i]);
+		++i;
+	}
 }
-
 
