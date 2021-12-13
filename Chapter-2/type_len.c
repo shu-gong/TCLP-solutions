b2fd2f96a245c7085726cefcfe679b1842de9cb7
SHU GONG <shugong.live@gmail.com>
2021-12-13 17:37:51 +0800

    some practice

---
+++ Chapter-2/type_len.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<limits.h>
+
+main() {
+	printf("\nBits of type char: %d\n\n", CHAR_BIT);
+	printf("Maximum numeric value of type char: %d\n", CHAR_MAX);
+	printf("Minimum numeric value of type char: %d\n\n", CHAR_MIN);
+
+}
