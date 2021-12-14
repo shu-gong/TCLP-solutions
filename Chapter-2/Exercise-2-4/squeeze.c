dc46216114461d0568aa3395f5725b4571b583b1
SHU GONG <shugong.live@gmail.com>
2021-12-14 22:15:03 +0800

    some practice

---
+++ Chapter-2/Exercise-2-4/squeeze.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+/* Write an alternative version of squeeze(s1, s2) that deletes each character in s1 that matches any character in the string s2*/
+
+
+main() {
+}
+
+void squeeze(char s1[], char s2[]) {
+	int i,j=0;
+	for(i=0; s1[i]!='\0'; ++i) {
+		for(j=0; s2[j]!='\0'; ++j) {
+			
+		}
+	}
+
+}
