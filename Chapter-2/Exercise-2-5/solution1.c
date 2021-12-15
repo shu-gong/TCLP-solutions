671e1ce0a1f923eea380e8210bc4430b2e2f670c
SHU GONG <shugong.live@gmail.com>
2021-12-15 19:41:35 +0800

    add soulution

---
+++ Chapter-2/Exercise-2-5/solution1.c
@@ -0,0 +1,12 @@
+int any(char s1[], char s2[])
+{
+	int i, j;
+
+	for(i = 0; s1[i] != '\0'; i++) {		/* for each s1[i] */
+		for(j = 0; s2[j] != '\0'; j++) {	/* for each s2[j] */
+			if(s1[i] == s2[j]) 		/* match found? */
+				return i;		/* no need for further code */
+		}
+	}
+	return -1;					/* no match */
+}
