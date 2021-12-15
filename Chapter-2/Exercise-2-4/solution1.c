edc44211515abcbae8384db5334d4bf28cd36fbd
SHU GONG <shugong.live@gmail.com>
2021-12-15 14:59:41 +0800

    add solutions

---
+++ Chapter-2/Exercise-2-4/solution1.c
@@ -0,0 +1,10 @@
+
+void squeeze2(char s[], char t[]) {
+    int i, j, k;
+    for (k = 0; t[k] != '\0'; k++) {
+        for (i = j = 0; s[i] != '\0'; i++)
+            if (s[i] != t[k])
+                s[j++] = s[i];
+        s[j] = '\0';
+    }
+}
