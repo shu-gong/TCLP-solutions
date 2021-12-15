2437a4c7d47201751ee282b56d53aa096c5abdba
SHU GONG <shugong.live@gmail.com>
2021-12-15 15:26:19 +0800

    some practice

--- Chapter-2/Exercise-2-4/solution1.c
+++ Chapter-2/Exercise-2-4/solution1.c
@@ -1,5 +1,14 @@
+#include<stdio.h>
+void squeeze(char s[], char t[]);
+main() {
+	char test1[] = "abacaca123aavas";
+	char test2[] = "abc";
 
-void squeeze2(char s[], char t[]) {
+	squeeze(test1, test2);
+	printf("%s", test1);
+}
+
+void squeeze(char s[], char t[]) {
     int i, j, k;
     for (k = 0; t[k] != '\0'; k++) {
         for (i = j = 0; s[i] != '\0'; i++)
--- Chapter-2/Exercise-2-4/squeeze.c
+++ Chapter-2/Exercise-2-4/squeeze.c
@@ -3,7 +3,7 @@
 void squeeze(char s1[], char s2[]);
 
 main() {
-	char test1[] = "aaabc123bcbacaab";
+	char test1[] = "aaasxkbc123bcbacaab";
 	char test2[] = "abc";
 
 	squeeze(test1, test2);
@@ -12,19 +12,12 @@ main() {
 
 void squeeze(char s1[], char s2[]) {
 	int i=0,j=0, k=0;
-	while(s1[i] != '\0') {
-		while(s2[j] != '\0') {
-			if(s1[i] == s2[j]) {
-				for(k=i; s1[k]!='\0'; ++k) {
-					s1[k] = s1[k+1];
-				}
-				j=0;
-			} 
-			else {
-				++j;
+	for(j=0; s2[j]!='\0'; ++j) {
+		for(i=k=0; s1[i]!='\0'; ++i) {
+			if(s1[i] != s2[j]) {
+				s1[k++] = s1[i];
 			}
 		}
-		j=0;
-		++i;
 	}
+	s1[k] = '\0';
 }
