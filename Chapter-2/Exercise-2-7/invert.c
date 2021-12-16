d02e7d699245a78572cb7b64f0f35608dc6e2cb2
SHU GONG <shugong.live@gmail.com>
2021-12-16 23:24:21 +0800

    add invert

---
+++ Chapter-2/Exercise-2-7/invert.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.*/
+
+main() {
+	
+}
+
+int invert(int x, int p, int n) {
+	return x ^ ((~(~0<<n)) << p+1-n);
+}
