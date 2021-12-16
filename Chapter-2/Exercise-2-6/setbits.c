af3a2110d29f0363b51e0c389c5674def7c1fa59
SHU GONG <shugong.live@gmail.com>
2021-12-16 10:18:48 +0800

    some practice

---
+++ Chapter-2/Exercise-2-6/setbits.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+/* Write a function setbits(s,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.*/
+
+
