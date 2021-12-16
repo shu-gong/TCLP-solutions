dbb2e24d3c81a3dc7534dc9e7fb110a92fb0fff7
SHU GONG <shugong.live@gmail.com>
2021-12-16 18:55:17 +0800

    some practice

--- Chapter-2/Exercise-2-6/setbits.c
+++ Chapter-2/Exercise-2-6/setbits.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
-/* Write a function setbits(s,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.*/
-unsigned getbits(unsigned x, int p, int n) {
-	return (x >> (p+1-n)) & ~(~0 << n);
-}
-
+/* Write a funciton setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.*/
+int setbits(x, p, n, y);
 main() {
-	int a;
-	a = getbits(5,4,3);
-	printf("%d", a);
+
 }
 
-
+int setbits(x,p,n,y) {
+	
+}
