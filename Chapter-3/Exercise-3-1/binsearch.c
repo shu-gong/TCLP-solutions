113b788a0074f6d9d786798c8ec39c24367c03b9
SHU GONG <shugong.live@gmail.com>
2021-12-17 16:47:02 +0800

    add binsearch

--- Chapter-3/Exercise-3-1/binsearch.c
+++ Chapter-3/Exercise-3-1/binsearch.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 /* Our binary search makes two tests inside the loop, when one would suffice (at the price of more tests outside). Write a version with only one test inside the loop and measure the difference in run-time.*/
+int binsearch(int x, int v[], int n);
 
 main() {
+	int var[] = {0,1,2,3,4,5,6,7,8,9};
+	printf("%d", binsearch(12, var, 10));
 
 }
 
@@ -10,11 +13,15 @@ int binsearch(int x, int v[], int n) {
 
 	low = 0;
 	high = n - 1;
-	while(low <= high) {
+	while(low < high) {
 		mid = (low+high)/2;
-		if (x < v[mid])
-			high = mid + 1;
+		if (x <= v[mid])
+			high = mid ;
 		else
-			low = mid;
+			low = mid + 1;
 	}
+	if (x == v[low])
+		return low;
+	else
+		return -1;
 }
---
+++ Chapter-3/Exercise-3-1/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+main() {
+	int s[] = {1,2,3,4};
+	int mid = 3/2;
+	printf("mid %d", mid);
+}
