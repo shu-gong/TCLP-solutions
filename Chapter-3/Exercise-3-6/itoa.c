81eac97def2823dd1c65d06147cbcb44808ab2ea
SHU GONG <shugong.live@gmail.com>
2021-12-19 19:14:38 +0800

    add itoa.c

---
+++ Chapter-3/Exercise-3-6/itoa.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+/* Write a version of itoa that accepts three arguments instread of two. The third argument is a minimum field width; the converted number must be padded with blanks on the left if necessary to make it wide enough.*/
+
+main() {
+	
+}
+
+void itoa()
