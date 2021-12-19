6b8f84c9cea715ad86ea6472c107cbe7f7bcc101
SHU GONG <shugong.live@gmail.com>
2021-12-19 18:58:58 +0800

    itob.c ok

--- Chapter-3/Exercise-3-5/itob.c
+++ Chapter-3/Exercise-3-5/itob.c
@@ -6,21 +6,21 @@ void itob(int n, char s[], int b);
 void reverse(char s[]);
 main() {
 	int n;
-	n = 127;
+	n = 120;
 	char test[MAXLINE];
 	itob(n, test, 2);
 	printf("%s", test);
 }
 
 void itob(int n, char s[], int b) {
-	int i,j,k, sign;
+	int i, sign;
 	if((sign = n) < 0) {
 		n = -n;
 	}
 	i = 0;
 	do {
 		s[i++] = n % b + '0';
-	} while (n / b > 0);
+	} while ((n /= b) > 0);
 	if (sign < 0)
 		s[i++] = '-';
 	s[i] = '\0';
@@ -30,8 +30,8 @@ void itob(int n, char s[], int b) {
 void reverse(char s[]) {
 	int i,j, temp;
 	for(i=0, j=strlen(s)-1; i < j; i++, j--) {
-		temp = s[j];
-		s[j] = s[i];
-		s[i] = temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
 }
