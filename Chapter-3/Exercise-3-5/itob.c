147e6cf3b0a1167f5afea6efed779dfbdba57048
SHU GONG <shugong.live@gmail.com>
2021-12-19 18:54:32 +0800

    itob,c

--- Chapter-3/Exercise-3-5/itob.c
+++ Chapter-3/Exercise-3-5/itob.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 /* Write the function itob(n, s, b) that converts the integer n into a base b character representation in the string s. In particular, itob(n,s,16) formats s as a hexadecimal integer in s*/
-
+#define MAXLINE 1000
 void itob(int n, char s[], int b);
 void reverse(char s[]);
 main() {
 	int n;
-
+	n = 127;
+	char test[MAXLINE];
+	itob(n, test, 2);
+	printf("%s", test);
 }
 
 void itob(int n, char s[], int b) {
@@ -17,9 +20,18 @@ void itob(int n, char s[], int b) {
 	i = 0;
 	do {
 		s[i++] = n % b + '0';
-	} while (s / b > 0)
-	s[i++] = '\0';
+	} while (n / b > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
 	reverse(s);
 }
 
-
+void reverse(char s[]) {
+	int i,j, temp;
+	for(i=0, j=strlen(s)-1; i < j; i++, j--) {
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+	}
+}
