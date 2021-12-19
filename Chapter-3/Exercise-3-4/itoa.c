9a905170f145056cb48829296679f99fe2d0a85e
SHU GONG <shugong.live@gmail.com>
2021-12-19 17:38:35 +0800

    add origin itoa

--- Chapter-3/Exercise-3-4/itoa.c
+++ Chapter-3/Exercise-3-4/itoa.c
@@ -1,4 +1,20 @@
 #include<stdio.h>
 /* In a two's complement number representation, our version of itoa does not handle the largest negative number, that is, the value of n qual to -(2^[wordsize-1]. Explain why not. Modify it to print that value correctly, regardless of the machine on which it runs.)*/
 
-void itoa()
+main() {
+	
+}
+
+void itoa() {
+	int i, sign;
+	if((sign = n) < 0)
+		n = -n;
+	i = 0;
+	do {
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
