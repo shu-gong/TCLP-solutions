f3ee095942e59150eea6bee941aa9e73ed10094f
SHU GONG <shugong.live@gmail.com>
2021-12-21 17:26:05 +0800

    modify itof.c

--- Chapter-4/Exercise-4-2/atof.c
+++ Chapter-4/Exercise-4-2/atof.c
@@ -25,7 +25,7 @@ double atof(char s[]) {
 		val = 10.0 * val + (s[i] - '0');
 	if (s[i] == '.')
 		i++;
-	for (power = 1.0; isdigit(s[i]; i++)) {
+	for (power = 1.0; isdigit(s[i]); i++) {
 		val = 10.0 * val + (s[i] - '0');
 		power *= 10;
 	}
