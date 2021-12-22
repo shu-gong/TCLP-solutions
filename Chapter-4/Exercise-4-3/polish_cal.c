b15ca02133bcf2dfd77038c48dff1b7702f476ba
SHU GONG <shugong.live@gmail.com>
2021-12-22 15:44:50 +0800

    add modulus to polish_cal.c

--- Chapter-4/Exercise-4-3/polish_cal.c
+++ Chapter-4/Exercise-4-3/polish_cal.c
@@ -48,6 +48,12 @@ main() {
 				else 
 					printf("error: zero division\n");
 				break;
+			case '%':
+				op2 = pop();
+				if (op2 != 0.0)
+					push(pop() % op2);
+				else
+					printf("error: zero division\n");
 			case '\n':
 				printf("\t%.8g\n", pop());
 				break;
