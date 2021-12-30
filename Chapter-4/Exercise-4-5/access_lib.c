49bb69ceb8aded4c6c99cb39e4cc2a785d6a11a8
SHU GONG <shugong.live@gmail.com>
2021-12-30 18:11:14 +0800

    add access to math.h

---
+++ Chapter-4/Exercise-4-5/access_lib.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int  main () {
+	char a[] = "abc";
+	char b[] = "bcd";
+	int c = trcmp(a,b);
+	printf("%d", c);
+	return 0;
+}
